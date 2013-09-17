f9a63bcf58a475c2bdd56023d6654bd1f789af40
Chris <c5keith90@gmail.com>
2013-09-17 09:03:55 -0600

    ok

---
+++ Multi.java
@@ -0,0 +1,62 @@
+public class Multi {
+	public static void main(String[] args) {
+	// 	int[][] a = { {1,2,3,4,5},
+	// 				  {6,7,8,9,10},
+	// 			 	  {11,12,13,14,15} };
+	// System.out.println("sum of the row 0 " + sumRow(a, 0));
+	// System.out.println("sum of the collum 0 " + sumcol(a, 0));
+	// System.out.println("the total of the matrix is " + sumTotal(a));
+	// System.out.println("the reverce of line 0 is " + reverseMatrix(a, 0));
+	// }
+
+}
+	public static int[] snakeDraft(int players ,int rounds) {
+		int[] a;
+		for (int i=0 ; ; ) {
+			for (; ; ) {
+				
+			}
+		}
+	}
+	
+
+	// public static int print(int[][] a) {
+	// // 	for (int r=0; r<a.length; r++) {
+	// // 		for (int c=0;c<a[r].length;c++) {
+	// // 			System.out.print(a[r][c] + " ");
+	// // 		}
+	// // 		System.out.println();
+	// // 	}
+	// // }
+	// public static int sumRow(int[][] a, int n){
+	// 	int sumr = 0;
+	// 	for (int c = 0; c < a[n].length; c++) {
+	// 		sumr = sumr + a[n][c];
+	// 		}
+	// 	return sumr;
+	// 	}
+	// public static int sumcol(int[][] a, int n){
+	// 	int sumc = 0;
+	// 	for (int i = 0;i < a.length;i++) {
+	// 	 	sumc = sumc + a[i][n];
+	// 	 }  
+	// 	return sumc;
+	// }
+	// public static int sumTotal(int[][] a){
+	// 	int sum = 0;
+	// 	for (int r=0; r<a.length; r++) {
+	// 		for (int c=0; c<a.length;c++ ) {
+	// 			sum = sum + a[r][c];
+	// 		}
+			
+	// 		}
+	// 	return sum;
+	// 	}
+	// public static int[] reverseMatrix(int[][] a, int n) {
+	//  int[] reverced = new int[a[n].length];
+	//  	for (int i = a[n].length - 1, r = 0; i >= 0 ;i--,r++ ) {
+	//  		reverced[r] = a[i][n];
+	// 	}
+	// 	return reverced;
+	// }	
+}
